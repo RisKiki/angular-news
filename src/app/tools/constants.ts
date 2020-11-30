2605895197462a7853e6ac060bc4442134e74403
Alexandre Disdier <alexandre.disdier@eisti.fr>
2020-11-30 17:51:52 +0100

    [ Constant ] - Change URL aAPI

--- src/app/tools/constants.ts
+++ src/app/tools/constants.ts
@@ -1,3 +1,3 @@
 export const NB_BY_ROWS_ACCUEIL = 4;
 export const MAX_LENGTH_DESCRIPTION_PREVIEW = 60;
-export const URL_API = "http://localhost:3000/";
+export const URL_API = "http://51.210.245.234:3000/";

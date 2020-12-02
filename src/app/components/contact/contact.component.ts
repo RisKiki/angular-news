83e7cae214f66ae93fa27586f0347b696d0f38fe
Alexandre Disdier <alexandre.disdier@eisti.fr>
2020-12-02 17:37:10 +0100

    [ Contact ] - add contact component in the footer

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  sended : boolean = false;
+
+  contactForm : FormGroup = new FormGroup({
+    name     : new FormControl('', [Validators.required]),
+    email    : new FormControl('', [Validators.required, Validators.email]),
+    subject  : new FormControl('', [Validators.required]),
+    message  : new FormControl('', [Validators.required])
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit() : void {
+    console.log(this.contactForm.value)
+    this.sended = true;
+
   }
 
 }

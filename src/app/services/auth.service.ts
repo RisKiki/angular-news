23ef6a4531f9ad1f656d022f7888a40f34dd9a12
Alexandre Disdier <alexandre.disdier@eisti.fr>
2021-01-27 14:50:09 +0100

    [ Auth Service ] - clear code

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
-import { RequestApiService } from './request-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  static tokenApi    : string = '';
-  private emailApi    : string = 'user1@user1';
-  private passwordApi : string = 'user1@user1';
-
-  constructor(
-    private requestApiService : RequestApiService
-  ) {}
+  constructor() {}
 
   static getToken() : string {
     return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZmNmZGU5NmQxZGIyODc2MjkyMWM0NDIiLCJpYXQiOjE2MTE0ODA2NzZ9.MkjD5LaBpcso3rgVr3EeROyMsayMS5mnbMcGCGgTnlo'
